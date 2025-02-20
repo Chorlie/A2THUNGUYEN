ea8dd83274a1deb1203d34623dc6b2a7c455fe0c
Thu <th858940@dal.ca>
2025-02-19 22:26:03 -0400

    Added and documented a copy of the cars dataset.

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Speed and Stopping Distances of Cars
+#'
+#' The data provide the speed and the distances to stop of cars which were recorded in 1920s.
+#'
+#' @format ## `cars`
+#' A data frame with 50 observations on 2 variables.
+#' \describe{
+#'   \item{speed}{numeric Speed (mph)}
+#'   \item{dist}{numeric Stopping distance (ft)}
+#' }
+#'
+#' @source Ezekiel, M. (1930) \emph{Methods of Correlation Analysis}. Wiley.
+#'
+#' @references McNeil, D. R. (1977) \emph{Interactive Data Analysis}. Wiley.
+#'
+#' @examples
+#' plot(cars, xlab = "Speed (mph)", ylab = "Stopping distance (ft)")
+"cars"
